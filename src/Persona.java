c47f8aa2fb839ac5e39fbcfd1e09114c9c4e84af
Diego Caudillo <diego.caudillo@uabc.edu.mx>
2019-03-28 16:42:43 -0700

    first commit

---
+++ src/Persona.java
@@ -0,0 +1,21 @@
+
+public class Persona {
+    int age;
+    String edad;
+
+    public Persona(){ }
+
+    public void getAge(){
+
+    }
+    public void getName(){
+
+    }
+    public void setName(String name) {
+        this.name = name;
+
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+}
